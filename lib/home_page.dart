47f4bad4c38932dc05c2aef674c8119addecb44b
Syed Yusuf Raza Zaidi <zaidiam95@gmail.com>
2022-09-22 06:49:30 +0500

    day 1 work - created homepage

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Center(child: Text("my app"),),
+      ),
+      drawer: Drawer(elevation: 10,),
+    );
+  }
+}
