8ee701a593dcfc48d45642542c13b995a8aedc19
Syed Yusuf Raza Zaidi <zaidiam95@gmail.com>
2022-09-23 05:28:57 +0500

    added new button with animated container, added login delay

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,8 +2,16 @@ import 'package:catalog_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String userName = "";
+  bool buttonTapped = false;
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +19,7 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-           // mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Image(
                 image: AssetImage("assets/images/login.png"),
@@ -21,7 +29,7 @@ class LoginScreen extends StatelessWidget {
                 height: 20,
               ),
               Text(
-                "Welcome!",
+                "Welcome, $userName",
                 style: TextStyle(
                     fontSize: 30,
                     fontFamily: GoogleFonts.pacifico().fontFamily),
@@ -36,10 +44,15 @@ class LoginScreen extends StatelessWidget {
                   children: [
                     TextFormField(
                       //autofocus: true,
+                      onChanged: (value) {
+                        setState(() {
+                          userName = value;
+                        });
+                      },
                       decoration: const InputDecoration(
-                          labelText: "Email",
+                          labelText: "Username",
                           //hintText: "enter email",
-                          icon: Icon(Icons.email)),
+                          icon: Icon(Icons.person)),
                     ),
                     TextFormField(
                       obscureText: true,
@@ -54,10 +67,41 @@ class LoginScreen extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              OutlinedButton(
+              /*OutlinedButton(
                 onPressed: () { Navigator.pushNamed(context, MyRoutes.homePageRoute);},
                 style: ElevatedButton.styleFrom(minimumSize: const Size(100, 45)),
                 child: const Text("Login"),
+              ),*/
+              InkWell(
+                onTap: () async {
+                  setState(() {
+                    buttonTapped = !buttonTapped;
+                  });
+
+                  await Future.delayed(const Duration(seconds: 1));
+                  Navigator.pushNamed(context, MyRoutes.homePageRoute);
+                },
+                child: AnimatedContainer(
+                  // Define how long the animation should take.
+                  duration: const Duration(seconds: 1),
+                  // Provide an optional curve to make the animation feel smoother.
+                  curve: Curves.elasticOut,
+                  height: 40,
+                  width: buttonTapped ? 60 : 120,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                      borderRadius: buttonTapped ? BorderRadius.circular(60) :BorderRadius.circular(5)),
+                  child: buttonTapped
+                      ? const Icon(
+                          Icons.thumb_up,
+                          color: Colors.white,
+                        )
+                      : const Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                ),
               )
             ],
           ),
